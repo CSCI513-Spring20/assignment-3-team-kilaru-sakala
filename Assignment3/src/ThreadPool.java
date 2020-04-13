9065c83c9312f0e64e2783c53b2b6723d463da3d
svssvarun <51722572+svssvarun@users.noreply.github.com>
2020-04-12 21:15:41 -0500

    Committing the ThreadPool class

---
+++ Assignment3/src/ThreadPool.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ThreadPool
+{
+	  //Thread pool size
+	  private final int poolSize;
+	
+	  //Internally pool is an array
+	  private final InternalTask[] workers;
+	
+	  // FIFO ordering
+	  private boolean isShutdown = false;
+	  private final LinkedBlockingQueue<Task> queue;
+	  private final ArrayList<Task> allTasks;
+	
+	  //Constructor of the class
+	  public ThreadPool(int poolSize){
+		  this.poolSize = poolSize;
+		  queue = new LinkedBlockingQueue<Task>();
+		  allTasks = new ArrayList<Task>();
+		  workers = new InternalTask[this.poolSize];
+		  for (int i = 0; i < poolSize; i++) {
+			  workers[i] = new InternalTask("Thread " + i);
+			  workers[i].start();
+		  }
+	  }
+	
+	  public void execute(Task task) {
+		  synchronized (queue) {
+			  queue.add(task);
+			  queue.notify();
+		  }
+	  }
+	
+	  public void waitForAllTasks() {
+		  boolean hasPendingTask = true;
+		  while (hasPendingTask) {
+			  hasPendingTask = false;
+			  for (Task task : allTasks) {
+				  if (task.getIsFinished() == false) {
+					  hasPendingTask = true;
+				  }
+			  }
+			  try {
+				  Thread.sleep(100);
+			  } catch (InterruptedException e) {
+				  System.out.println("Exception happened while wait for threads to complete: " + e.getMessage());
+			  }
+		  }
+	  }
+	
+	  public void shutdown() {
+		  this.isShutdown = true;
+	  }
+	
+	  private class InternalTask extends Thread {
+		  public InternalTask(String name) {
+			    super(name);
+		  }
+		  @Override
+		  public void run() {
+			  Task task;
+			  while (true) {
+				  synchronized (queue) {
+					  if (isShutdown && queue.isEmpty()) {
+						  break;
+					  }
+					  while (queue.isEmpty()) {
+						  try {
+							  queue.wait();
+						  } catch (InterruptedException e) {
+							  System.out.println("An error occurred while queue is waiting: " + e.getMessage());
+						  }
+					  }
+					  task = (Task) queue.poll();
+				  }	  
+				  try {
+					  task.run();
+					  task.setIsFinished();
+				  } catch (RuntimeException e) {
+					  System.out.println("Thread pool is interrupted due to an issue: " + e.getMessage());
+				  }
+			  }
+		  }
+	  	}
+
+}
